12cf708e25392ae9b2f36b2014c9ad3753ad1c09
caitz1310 <142206866+caitz1310@users.noreply.github.com>
2023-12-04 21:25:19 +0200

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Caitlin_Hermanus_20232198_SLD521_SA.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
